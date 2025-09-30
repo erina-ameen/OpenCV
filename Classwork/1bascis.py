413c1af0afa08b850391eebd726112dec8721690
erina-ameen <erina.ameen@gmail.com>
2025-09-30 15:25:34 +0100

    1bascis.py

--- Classwork/1bascis.py
+++ Classwork/1bascis.py
@@ -11,3 +11,15 @@ store=cv2.imread("flower.png", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("screen", store)
 
 cv2.waitKey(0)
+
+#saving a modified image
+import os
+path=r"D:\ERINA\Jet Learn\VS Code OpenCV\Classwork"
+img=cv2.imread("flower.png", 0)
+cv2.imshow("screen", img)
+
+cv2.waitKey(0)
+
+os.chdir(path)
+cv2.imwrite("flowergray.png", img)
+print("Saved!")
