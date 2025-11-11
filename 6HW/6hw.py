23bbdd6a12958d7ec1e8e2d36e6acd7f638a53e5
erina-ameen <erina.ameen@gmail.com>
2025-11-11 15:06:34 +0000

    Add files via upload

---
+++ 6HW/6hw.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+circles=cv2.imread(r"D:\ERINA\Jet Learn\VS Code OpenCV\HW\6HW\circles.jpg")
+
+#Converting to grayscale
+circles_grayscale=cv2.cvtColor(circles, cv2.COLOR_BGR2GRAY)
+circles_grayscale_blur=cv2.blur(circles_grayscale, (4,4))
+detected_circles=cv2.HoughCircles(circles_grayscale_blur, cv2.HOUGH_GRADIENT, 1, 20, 3, 30)
+
+if detected_circles is not None:
+    detected_circles=np.uint16(detected_circles)
+    for i in detected_circles[0,:]:
+        x,y,r=i[0], i[1], i[2]
+        cv2.circle(circles, (x,y), r, (255,150,255), 3)
+        cv2.circle(circles, (x,y), 1, (255,150,255), 3)
+        cv2.imshow("Shape Detection", circles)
+        cv2.waitKey(0)
