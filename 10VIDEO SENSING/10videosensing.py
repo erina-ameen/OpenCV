c3ffa8e4d9a4d71e9968ce5f7f8b639f06f8597f
erina-ameen <erina.ameen@gmail.com>
2025-12-18 14:55:55 +0000

    10videosensing.py
    Updated balloon positioning to avoid edge overlap and added motion detection for balloon interaction.

--- 10VIDEO SENSING/10videosensing.py
+++ 10VIDEO SENSING/10videosensing.py
@@ -10,9 +10,9 @@ balloon_num=10
 score=0
 
 for i in range(balloon_num):
-    x=random.randint(0, 500)
-    y=random.randint(0, 500)
-    radius=random.randint(20-70)
+    x=random.randint(20, 580)
+    y=random.randint(20, 420)
+    radius=random.randint(20,70)
     balloon_list.append({"x":x, "y":y, "radius":radius, "popped":False})
 
 font=cv2.FONT_HERSHEY_SIMPLEX
@@ -23,3 +23,32 @@ while True:
 
     _,thresh=cv2.threshold(blurred, 10, 255, cv2.THRESH_BINARY)
     dilated_img=cv2.dilate(thresh, None, iterations=3)
+
+    #finding movement
+    contours, _=cv2.findContours(dilated_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    frame=frame1.copy()
+    for i in balloon_list:
+        if not i["popped"]:
+            b=random.randint(0,255)
+            g=random.randint(0,255)
+            r=random.randint(0,255)
+            cv2.circle(frame, [i["x"], i["y"]], i["radius"], (b,g,r), -1)
+    
+    #checking motion overlaps with balloon
+    for c in contours:
+        if cv2.contourArea(c)<1500:
+            continue
+    
+    frame1=frame2
+    tf, frame2=camera.read()
+
+    cv2.putText(frame, "Score: "+str(score), (10,30), font, 1, (56, 32, 67), 2)
+
+    cv2.imshow("screen", frame)
+
+    key=cv2.waitKey(10)
+    if key==27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
