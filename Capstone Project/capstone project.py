eb3be0b58d8c3e31fbf29efec10e03155035c007
erina-ameen <erina.ameen@gmail.com>
2026-01-06 15:23:33 +0000

    capstoneproject
    Added text overlay to video frames during generation.

--- Capstone Project/capstone project.py
+++ Capstone Project/capstone project.py
@@ -43,6 +43,8 @@ def generate():
     height, width, layers=frame.shape
     vid_store=cv2.VideoWriter(vid_name, 0, 0.5, (width, height))
     for img in slide_images:
+        frame=cv2.imread(os.path.join(".", img))
+        cv2.putText(frame, "Hobby Ideas", (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2,cv2.LINE_AA)
         vid_store.write(cv2.imread(os.path.join(".", img)))
     cv2.destroyAllWindows()
     vid_store.release()
