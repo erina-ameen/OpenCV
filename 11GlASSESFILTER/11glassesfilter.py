a6a45b1ac6fcf9dbaf4b3a43e3ee64185797419e
erina-ameen <erina.ameen@gmail.com>
2026-01-13 15:56:08 +0000

    Add files via upload

---
+++ 11GlASSESFILTER/11glassesfilter.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy
+
+#Loading face detector
+face_detector=cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+glasses1_read=cv2.imread(r"D:\ERINA\Jet Learn\VS Code OpenCV\11GlASSESFILTER\glasses1.png", cv2.IMREAD_UNCHANGED)
+glasses2_read=cv2.imread(r"D:\ERINA\Jet Learn\VS Code OpenCV\11GlASSESFILTER\glasses2.png", cv2.IMREAD_UNCHANGED)
+glasses3_read=cv2.imread(r"D:\ERINA\Jet Learn\VS Code OpenCV\11GlASSESFILTER\glasses3.png", cv2.IMREAD_UNCHANGED)
+camera=cv2.VideoCapture(0)
+filter_type="Original"
+print("Press keys to apply filter")
+print("0 - Original")
+print("1 - Glasses 1")
+print("2 - Glasses 2")
+print("3 - Glasses 3")
+print("Esc - Quit")
+
+#function for overlay
+def overlay(bg, img, x, y):
+    bg_height, bg_width=bg.shape[:2]
+    img_height, img_width=img.shape[:2]
+    if x+img_width>bg_width or y+img_height>bg_height:
+        return bg
+    transparancy=img[:,:,3]/255.0
+    for i in range(3):
+        bg[y:y+img_height, x:x+img_width, i]
+
+while True:
+    ret, frame=camera.read()
+    if not ret:
+        break
+    cv2.imshow("Screen", frame)
+    #setting filter according to the keys
+    key=cv2.waitKey(1)
+    if key==ord("0"):
+        filter_type="Original"
+    elif key==ord("1"):
+        filter_type="Glasses 1"
+    elif key==ord("2"):
+        filter_type="Glasses 2"
+    elif key==ord("3"):
+        filter_type="Glasses 3"
+    elif key==27:
+        break
