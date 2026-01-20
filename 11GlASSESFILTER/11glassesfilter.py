bfc75d5755806164cfd255d3f32ac9a9153f26f7
erina-ameen <erina.ameen@gmail.com>
2026-01-20 15:37:14 +0000

    11glassesfilter.py
    Added functionality to apply glasses filter to detected faces.

--- 11GlASSESFILTER/11glassesfilter.py
+++ 11GlASSESFILTER/11glassesfilter.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 
 #Loading face detector
 face_detector=cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
@@ -8,6 +8,9 @@ glasses2_read=cv2.imread(r"D:\ERINA\Jet Learn\VS Code OpenCV\11GlASSESFILTER\gla
 glasses3_read=cv2.imread(r"D:\ERINA\Jet Learn\VS Code OpenCV\11GlASSESFILTER\glasses3.png", cv2.IMREAD_UNCHANGED)
 camera=cv2.VideoCapture(0)
 filter_type="Original"
+actual_h1, actual_w1=glasses1_read.shape[:2]
+aspect_ratio1=actual_w1/actual_h1
+
 print("Press keys to apply filter")
 print("0 - Original")
 print("1 - Glasses 1")
@@ -29,7 +32,18 @@ while True:
     ret, frame=camera.read()
     if not ret:
         break
+    if filter_type=="Glasses 1":
+        grayscale=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces_list=face_detector.detectMultiScale(grayscale, 1.1, 3)
+        for (x, y, width, height) in faces_list:
+            glass_w=width
+            glass_h=int(glass_w/aspect_ratio1)
+            resized_glasses1=cv2.resize(glasses1_read, (glass_w, glass_h))
+            frame=overlay(frame, resized_glasses1, x, y)
+    if isinstance(frame, np.ndarray) and len(frame.shape)==2:
+        frame=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Screen", frame)
+
     #setting filter according to the keys
     key=cv2.waitKey(1)
     if key==ord("0"):
@@ -42,3 +56,5 @@ while True:
         filter_type="Glasses 3"
     elif key==27:
         break
+camera.release()
+cv2.destroyAllWindows()
